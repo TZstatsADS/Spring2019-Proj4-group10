85992ae861fc39970f0afbf39b8747077a586fdd
Xiaoxi Zhao <xz2740@columbia.edu>
2019-04-07 10:32:48 -0400

    feature4-9

--- lib/features.R
+++ lib/features.R
@@ -2,8 +2,10 @@ if (!require("stringr")) {
   install.packages("stringr")
 }
 
+library(stringr)
+
 feature1 <- function(word) {
-  return(str_length(input));
+  return(str_length(word))
 }
 
 
@@ -27,8 +29,67 @@ feature2 <- function(word) {
 }
 
 feature3 <- function(word) {
-  return(length(grep("[^[:alnum:]=\\.]", strsplit(word,"")[[1]])));
+  s<-length(grep("[^[:alnum:]=\\.]", strsplit(word,"")[[1]]))
+  l<-str_length(word)
+  return(c(s,s/l))
 }
 
+feature4 <- function(word){
+  d<-length(grep("[0-9]", strsplit(word,"")[[1]]))
+  l<-str_length(word)
+  return(c(d,d/l)) 
+}
 
+feature5 <- function(word){
+  low<-length(grep("[a-z]", strsplit(word,"")[[1]]))
+  upp<-length(grep("[A-Z]", strsplit(word,"")[[1]]))
+  l<-str_length(word)
+  return(c(low,upp,low/l,upp/l)) 
+}
 
+feature6 <- function(word){
+  l<-str_length(word)
+  cur_count <- c()
+  i<-1
+  while (TRUE) {
+    cur_letter <- substr(word,i,i)
+    cur_count[i] <- 1
+    j <- i + 1
+    if(j==l+1){
+      break
+    }
+    while (substr(word,j,j)==cur_letter) {
+      cur_count[i] <- cur_count[i] + 1
+      j <- j + 1
+    }
+    i<-j
+    if(i==l+1){
+      break
+    }
+  }
+  count <- max(cur_count,na.rm = T)
+  if(count<=3){
+    return(0)
+  }else{
+    return(count/l)
+  }
+}
+
+feature7<- function(word){
+  l_alpha<-length(grep("[[:alnum:]=\\.]", strsplit(word,"")[[1]]))
+  l<-nchar(word)
+  k<-l-l_alpha
+  return(ifelse(k>l_alpha,1,0))
+}
+
+feature8<- function(word){
+  consec_con<-str_count(word,"[bcdfghjklmnpqrstvxzwyBCDFGHJKLMNPQRSTVXZWY]{6,}")
+  return(ifelse(consec_con>=1,1,0))
+}
+
+feature9<- function(word){
+  l<-nchar(word)
+  trim_word <- substr(word,2,(l-1))
+  non_alnum<-str_count(trim_word,"[^[:alnum:]]")
+  return(ifelse(non_alnum>=2,1,0))
+}
